b1683055f9c3264f70e9868a840930c294895827
Raphael Ferreira <raphatec1993@outlook.com>
2022-06-29 01:20:50 -0300

    first commit

---
+++ javaScript.js
@@ -0,0 +1,15 @@
+function cadastro(){
+    
+    let name = document.getElementById('nome').Value
+    let senha = document.getElementById('senha').Value
+    const botao = document.getElementById('button')
+
+    if(name == 'rapha@gmail.com' && senha == '123'){
+        alert("Digite algo")
+    }else{
+        window.location.href = "https://google.com"
+    }
+
+}
+
+
